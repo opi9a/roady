4bead0e1be50545132f942fcaba33a294a1a3ab7
marvin <opi9a@gavinroberts.com>
2023-07-05 09:31:14 +0100

    layout

--- src/roady/make_pdf.py
+++ src/roady/make_pdf.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import requests
 import shutil
+import re
 from datetime import datetime, timedelta
 from reportlab.lib.pagesizes import A4
 
@@ -15,15 +16,25 @@ def make_front_page(stages, img_url, canvas, imgs_dir=None):
     """
     List of stages and map
     """
-    top = 28
+    top = 29
     bottom = 2
     left = 2
     right = 21
     width = right - left
-    height = top - bottom
 
-    route_h = 14
+    title_h = 1.5
+    route_h = 17
 
+    # title - have to infer tour and year
+    tour, year = re.search("images/(\S*)/(\d*)/route.jpg", img_url).groups()
+    tour = tour.title().replace('-', ' ').replace("De", "de")
+
+    canvas.setFont("Helvetica-Bold", 20)
+    canvas.drawString((left + 5) * cm, (top - title_h) * cm,
+                      text=f"{tour} {year}")
+
+
+    # route map
     i_dict = get_image(img_url, imgs_dir)
 
     h, w = scale_image(i_dict['height'], i_dict['width'],
@@ -31,13 +42,11 @@ def make_front_page(stages, img_url, canvas, imgs_dir=None):
 
     canvas.drawInlineImage(
         i_dict['path'].as_posix(),
-        x = 2 * cm,
-        y = 15 * cm,
+        x = left + ((width - w)/2) * cm,
+        y = (top - h - title_h - 1) * cm,
         height = h * cm,
         width = w * cm,
     )
-
-    canvas.setFont("Courier", 8)
 
     new_stages = []
 
@@ -56,34 +65,53 @@ def make_front_page(stages, img_url, canvas, imgs_dir=None):
             new_stages.append({'type': 'rest day', 
                                'date': new_dt.strftime(fmt)})
         new_stages.append(stage)
+        new_stages[-1]['stage_no'] = str(new_stages[-1]['stage_no'])
+        new_stages[-1]['distance'] = str(new_stages[-1]['distance'])
 
         prev_stage_dt = dt
 
 
-
-    y0 = top - route_h - 1
+    # stages
+    y0 = top - route_h - title_h + 2 # this is frigged tbf
     lh = min(1, (y0 - bottom) / len(new_stages))
-    i = 0
+
+    # column headings
+    col_xs = {
+        'date': 2,
+        'stage_no': 5.5,
+        'from_to': 6.5,
+        'distance': 15,
+        'type': 17,
+    }
+
+    canvas.setFont("Helvetica-Bold", 10)
+
+    for col, x in col_xs.items():
+        if col == 'from_to': text = "from-to"
+        elif col == 'stage_no': text = "no"
+        elif col == 'distance': text = "km"
+        else: text = col
+        canvas.drawString(x * cm, y0 * cm, text=text)
+
+    # stages
+    canvas.setFont("Helvetica", 10)
+
+    i = 1
     for stage in new_stages:
 
-        if stage['type'] == 'rest day':
-            line = f"{stage['date']:<20}  Rest Day"
-            
-        else:
-            line = (
-                f"{stage['date']:<20} {stage['stage_no']:>3} "
-                f"{stage['from_to']:<45} {stage['distance']:>4} "
-                    f"{stage['type']:>12}"
-                )
+        y = (y0 - (lh * i))
 
-        canvas.drawString(
-            x = 2 * cm,
-            y = (y0 - (lh * i)) * cm,
-            text = line,
-        )
+        if stage['type'] == 'rest day':
+            canvas.drawString(col_xs['date'] * cm, y * cm, text=stage['date'])
+            canvas.drawString(col_xs['stage_no'] * cm, y * cm,
+                              text=f'{"-"*48} REST DAY {"-"*48}')
+            i += 1
+            continue
+
+        for col, x in col_xs.items():
+            canvas.drawString(x * cm, y * cm, text=stage[col])
 
         i += 1
-
 
     canvas.showPage()
 
@@ -109,11 +137,9 @@ def make_pdf(stage_dict, canvas=None, outpath=None, imgs_dir=None):
     left = 2
     right = 18
 
-    title = (f"Stage {stage_dict['stage_no']}: {stage_dict['date']} "
-             f"----- {stage_dict['from_to']}"
-             f"  {stage_dict['type']}  : {stage_dict['distance']:.1f} km"
-            )
-    canvas.drawString(left*cm, top*cm, title)
+    canvas.setFont("Helvetica-Bold", 20)
+    title = (f"Stage {stage_dict['stage_no']} - {stage_dict['date']}")
+    canvas.drawString((left + 4.5)*cm, top*cm, title)
 
     i_dict = {}
 
@@ -123,7 +149,7 @@ def make_pdf(stage_dict, canvas=None, outpath=None, imgs_dir=None):
     h, w = scale_image(
         i_dict['route']['height'],
         i_dict['route']['width'],
-        max_h = 15,
+        max_h = 14,
         max_w = right - left,
     )
 
@@ -145,7 +171,7 @@ def make_pdf(stage_dict, canvas=None, outpath=None, imgs_dir=None):
     canvas.drawInlineImage(
         i_dict['profile']['path'].as_posix(),
         x = left*cm,
-        y = (top - (2 + i_dict['profile']['plot_height']))*cm,
+        y = (top - (1 + i_dict['profile']['plot_height']))*cm,
         height = i_dict['profile']['plot_height'] * cm,
         width = i_dict['profile']['plot_width'] * cm,
     )
@@ -154,7 +180,7 @@ def make_pdf(stage_dict, canvas=None, outpath=None, imgs_dir=None):
         y = top - (
                 2 # allowance for title
                 + i_dict['profile']['plot_height']
-                + 2 # gap
+                + 0 # gap
                 + i_dict['route']['plot_height']
             )
         canvas.drawInlineImage(
